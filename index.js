8051808e25d20b10be61b2fe0c39759c6a28d116
Shohidul Arifin <shohidularifin@gmail.com>
2024-01-20 01:25:57 -0800

    routes changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://vercel.com/arifin1987/breakpoint-art-project"],
+    origin: ["https://breakpoint-art-project.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true,
   })
